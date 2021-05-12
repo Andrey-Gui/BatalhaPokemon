88c0eb574c43eec3b6b15518a124b3f99b7f96f6
Felipe_Fit <83964268+Felipe-Fiti@users.noreply.github.com>
2021-05-11 21:35:10 -0300

    Mais ataques foram adicionados

--- Pokemon/BatalhaPokemon.java
+++ Pokemon/BatalhaPokemon.java
@@ -9,46 +9,56 @@ public class BatalhaPokemon{
 static int ataqueJogador1() {
   leitor = new Scanner(System.in);
   System.out.println("Escolha o seus Três Pokemons:");
+  System.out.println("=============================");
+  System.out.println("=============================");
   System.out.println("1 - Vaporeon = (Água)");
   System.out.println("2 - Flareon = (Fogo)");
   System.out.println("3 - Jolteon = (Raio)");
   System.out.println("4 - Eevee = (Normal)");
   System.out.println("5 - Umbreon = (Escuridão)");
   System.out.println("6 - Leafeon = (Fantasma)");
+  System.out.println("============================");
+  System.out.println("============================");
   System.out.println("Os pokemons escolhidos são:");
   return leitor.nextInt();
  }
 
  static int ataqueJogador2() {
   System.out.println("Escolha o seus Três Pokemons:");
+  System.out.println("=============================");
+  System.out.println("=============================");
   System.out.println("1 - Vaporeon = (Água)");
   System.out.println("2 - Flareon = (Fogo)");
   System.out.println("3 - Jolteon = (Raio)");
   System.out.println("4 - Eevee = (Normal)");
   System.out.println("5 - Umbreon = (Escuridão)");
   System.out.println("6 - Leafeon = (Fantasma)");
+  System.out.println("=============================");
+  System.out.println("=============================");
   System.out.println("Os pokemons escolhidos são:");
   return leitor2.nextInt();
  }
 
  static void imprimeHP(int hpJogador1, int hpJogador2) {
   System.out.println("====================");
+  System.out.println("====================");
   System.out.println("- HP Jogador 1: " + hpJogador1);
   System.out.println("====================");
   System.out.println("====================");
   System.out.println("- HP Jogador 2: " + hpJogador2);
   System.out.println("====================");
+  System.out.println("====================");
  }
 
  static void batalha() {
-  int hpJogador1 = 200;
-  int hpJogador2 = 200;
+  int hpJogador1 = 250;
+  int hpJogador2 = 250;
   int escolhaAtaque;
 
   while (hpJogador1 > 0) {
 
-   System.out.println("====================");
-   System.out.println("     INICIO ");
+   System.out.println("====================\n");
+   System.out.println("\n      INICIO      \n");
    System.out.println("====================\n");
 
    while (hpJogador1 > 0 && hpJogador2 > 0) {
@@ -58,29 +68,30 @@ static int ataqueJogador1() {
   case 1: {
         System.out.println("Jogador 1 aplicou o ataque Water Gun.");
         hpJogador2 -= 10;
+        break;   
            }
   case 2: {
         System.out.println("Jogador 1 aplicou um ataque Hydro Pump.");
         hpJogador2 -= 30;
-       }
-    default: System.out.println("Opcao invalida");
+        break;
+        }
   case 3:{
-      System.out.println("Jogador 1 aplicou um ataque z.");
+      System.out.println("Jogador 1 aplicou um ataque Flamethrower.");
       hpJogador2 -= 30;
       break;
          }
   case 4:{
-      System.out.println("Jogador 1 aplicou um ataque a.");
+      System.out.println("Jogador 1 aplicou um ataque HitKill.");
       hpJogador2 -= 40;
       break;
          }
   case 5:{
-      System.out.println("Jogador 1 aplicou um ataque b.");
+      System.out.println("Jogador 1 aplicou um ataque Attack of Shadows.");
       hpJogador2 -= 50;
       break;
          }
   case 6:{
-      System.out.println("Jogador 1 aplicou um ataque c.");
+      System.out.println("Jogador 1 aplicou um ataque Ilusion.");
       hpJogador2 -= 60;
       break;
          }     
@@ -91,27 +102,30 @@ static int ataqueJogador1() {
      case 1:{
          System.out.println("Jogador 2 aplicou um ataque Ember.");
          hpJogador1 -= 15;
+         break;
             }
      case 2:{
          System.out.println("Jogador 2 aplicou um ataque 	Fire Blast.");
          hpJogador1 -= 10;
+         break;
             }
      case 3:{
          System.out.println("Jogador 2 aplicou um ataque  Flamethrower.");
          hpJogador1 -= 32;
+         break;
            }
     case 4:{
-         System.out.println("Jogador 1 aplicou um ataque a.");
+         System.out.println("Jogador 2 aplicou um ataque  HitKill.");
          hpJogador2 -= 40;
          break;
            }
     case 5:{
-        System.out.println("Jogador 1 aplicou um ataque b.");
+        System.out.println("Jogador 2 aplicou um ataque  Attack of Shadows.");
         hpJogador2 -= 50;
         break;
            }
     case 6:{
-        System.out.println("Jogador 1 aplicou um ataque c.");
+        System.out.println("Jogador 2 aplicou um ataque  Ilusion.");
         hpJogador2 -= 60;
         break;
        }     
